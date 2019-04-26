7486846ee00560f98eb791d4465433abb5a9c2e2
John <37978984+johnridesabike@users.noreply.github.com>
2019-04-26 06:39:09 -0400

    begun converting layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,31 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this
-default starter. This barebones starter ships with the main Gatsby
-configuration files you might need.`,
-        author: `@gatsbyjs`
+        title: "John Jackson",
+        description: "Librarian portfolio & other projects.",
+        author: "John Jackson"
     },
     plugins: [
-        `gatsby-plugin-react-helmet`,
+        "gatsby-plugin-react-helmet",
         {
-            resolve: `gatsby-source-filesystem`,
+            resolve: "gatsby-source-filesystem",
             options: {
-                name: `images`,
+                name: "images",
                 path: `${__dirname}/src/images`
             }
         },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
         {
-            resolve: `gatsby-plugin-manifest`,
+            resolve: "gatsby-plugin-manifest",
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
+                name: "gatsby-starter-default",
+                short_name: "starter",
+                start_url: "/",
+                background_color: "#663399",
+                theme_color: "#663399",
+                display: "minimal-ui",
                 // This path is relative to the root of the site.
-                icon: `src/images/gatsby-icon.png`
+                icon: "src/images/gatsby-icon.png"
             }
         },
         // this (optional) plugin enables Progressive Web App + Offline
@@ -35,12 +33,12 @@ configuration files you might need.`,
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
         {
-            resolve: `gatsby-source-filesystem`,
+            resolve: "gatsby-source-filesystem",
             options: {
                 path: `${__dirname}/content`,
                 name: "markdown-pages"
             }
         },
-        `gatsby-transformer-remark`
+        "gatsby-transformer-remark"
     ]
 };
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,12 +5,22 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React, {Fragment} from "react";
+import PropTypes from "prop-types";
+import {StaticQuery, graphql} from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "normalize.css";
+import "./styles/elements.css";
+import "./styles/typography.css";
+import "./styles/header-footer.css";
+import "./styles/navigation.css";
+import "./styles/widgets.css";
+import "./styles/content.css";
+import "./styles/wp-blocks.css";
+import "./styles/accessibilitiy-clearings.css";
+import "./styles/media.css";
+// import "./layout.css";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,36 +28,37 @@ const Layout = ({ children }) => (
       query SiteTitleQuery {
         site {
           siteMetadata {
-            title
+            title,
+            description
           }
         }
       }
     `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+    render={(data) => (
+      <Fragment>
+        <div id="page" className="site">
+            <a className="skip-link screen-reader-text" href="#content">
+                Skip to content
+            </a>
+            <Header
+                siteTitle={data.site.siteMetadata.title}
+                siteDescription={data.site.siteMetadata.description} />
+            <div id="content" class="site-content">
+                <main>{children}</main>
+            </div>
+            <footer>
+                © {new Date().getFullYear()}, Built with
+                {" "}
+                <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
         </div>
-      </>
+      </Fragment>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
