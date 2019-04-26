68e6e956f7dc5d5457cfcc8e3ec0986c3a2ffa56
John <37978984+johnridesabike@users.noreply.github.com>
2019-04-26 05:55:04 -0400

    housekeeping

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,39 +5,39 @@
  */
 
 // You can delete this file if you're not using it
-const path = require("path")
+const path = require("path");
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+exports.createPages = function ({actions, graphql}) {
+    const {createPage} = actions;
 
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.jsx`)
+    const blogPostTemplate = path.resolve(`src/templates/blogTemplate.jsx`);
 
-  return graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
+    return graphql(`
+        {
+          allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            limit: 1000
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  path
+                }
+              }
             }
           }
         }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
+    `).then(function (result) {
+        if (result.errors) {
+            return Promise.reject(result.errors);
+        }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: blogPostTemplate,
-        context: {}, // additional data can be passed via context
-      })
-    })
-  })
-}
+        result.data.allMarkdownRemark.edges.forEach(function ({node}) {
+            createPage({
+                path: node.frontmatter.path,
+                component: blogPostTemplate,
+                context: {} // additional data can be passed via context
+            });
+        });
+    });
+};
