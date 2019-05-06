188addfb88cfba1e8f7b9400b04887871a70a017
John <37978984+johnridesabike@users.noreply.github.com>
2019-05-06 13:00:23 -0400

    css updates

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -28,204 +28,202 @@ function IndexPage() {
     );
     return (
         <Layout>
-            <div id="primary" className="content-area single">
-                <main id="main" className="site-main post">
-                    <div className="entry-content">
-                        <SEO
-                            title="Home"
-                            keywords={["librarian", "software", "woodworking"]}
-                        />
-                        {/* <h1>Hi people</h1>
-                        <p>Welcome to your new Gatsby site.</p>
-                        <p>Now go build something great.</p>
+            <SEO
+                title="Home"
+                keywords={["librarian", "software", "woodworking"]}
+            />
+            <main id="main" className="site-main">
+                <div className="page-content">
+                    {/* <h1>Hi people</h1>
+                    <p>Welcome to your new Gatsby site.</p>
+                    <p>Now go build something great.</p>
+                    <div
+                        style={{
+                            maxWidth: "300px",
+                            marginBottom: "1.45rem"
+                        }}
+                    >
+                        <Image />
+                    </div>
+                    <Link to="/page-2/">Go to page 2</Link> */}
+                    <div
+                        className="wp-block-columns has-2-columns"
+                        style={{textAlign: "center"}}
+                    >
+                        <div className="wp-block-column">
+                            <img
+                                src={john2018}
+                                alt="Portrait of John"
+                                height="256"
+                                width="256"
+                            />
+                        </div>
                         <div
+                            className="wp-block-column"
                             style={{
-                                maxWidth: "300px",
-                                marginBottom: "1.45rem"
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center"
                             }}
                         >
-                            <Image />
+                            <p className="has-large-font-size">
+                                Hi, I'm John Jackson.
+                            </p>
                         </div>
-                        <Link to="/page-2/">Go to page 2</Link> */}
-                        <div
-                            className="wp-block-columns has-2-columns"
-                            style={{textAlign: "center"}}
-                        >
-                            <div className="wp-block-column">
-                                <img
-                                    src={john2018}
-                                    alt="Portrait of John"
-                                    height="256"
-                                    width="256"
-                                />
-                            </div>
-                            <div
-                                className="wp-block-column"
-                                style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "center"
-                                }}
-                            >
-                                <p className="has-large-font-size">
-                                    Hi, I'm John Jackson.
-                                </p>
-                            </div>
-                        </div>
-                        <p>
-                            I’m a public library staffer who is interested in
-                            digital library technologies, web development, and
-                            libraries of all types. I like to bike, write,
-                            build, and tinker.
-                        </p>
-                        <div className="wp-block-columns has-2-columns">
-                            <div className="wp-block-column">
-                                <p style={{textAlign: "center"}}>
-                                    <strong>
-                                        Hiring? Here’s a bit about me:
-                                    </strong>
-                                </p>
-                                <p>
-                                    <span role="img" aria-label="check">
-                                        ✔️
-                                    </span>{" "}
-                                    MLIS Graduate.
-                                </p>
-                                <p>
-                                    <span role="img" aria-label="check">
-                                        ✔️
-                                    </span>{" "}
-                                    Working in libraries since 2015.
-                                </p>
-                                <p>
-                                    <span role="img" aria-label="check">
-                                        ✔️
-                                    </span>{" "}
-                                    Experience in adult services programming,
-                                    web development, data management, customer
-                                    service, and more.
-                                </p>
-                                <div className="wp-block-button aligncenter is-style-squared">
-                                    <Link
-                                        className="wp-block-button__link"
-                                        to="/resume/"
-                                    >
-                                        View my full résumé
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="wp-block-column">
-                                <p style={{textAlign: "center"}}>
-                                    <strong>Let’s get in touch.</strong>
-                                </p>
-                                <p>
-                                    Questions, comments, job offers, jokes,
-                                    poems, or just to say “hi.”{" "}
-                                </p>
-                                <div className="wp-block-button aligncenter is-style-squared">
-                                    <a
-                                        className="wp-block-button__link"
-                                        href="mailto:jbpjackson@icloud.com"
-                                    >
-                                        <MailIcon /> Email me
-                                    </a>
-                                </div>
+                    </div>
+                    <p>
+                        I’m a public library staffer who is interested in
+                        digital library technologies, web development, and
+                        libraries of all types. I like to bike, write,
+                        build, and tinker.
+                    </p>
+                    <div className="wp-block-columns has-2-columns">
+                        <div className="wp-block-column">
+                            <p style={{textAlign: "center"}}>
+                                <strong>
+                                    Hiring? Here’s a bit about me:
+                                </strong>
+                            </p>
+                            <p>
+                                <span role="img" aria-label="check">
+                                    ✔️
+                                </span>{" "}
+                                MLIS Graduate.
+                            </p>
+                            <p>
+                                <span role="img" aria-label="check">
+                                    ✔️
+                                </span>{" "}
+                                Working in libraries since 2015.
+                            </p>
+                            <p>
+                                <span role="img" aria-label="check">
+                                    ✔️
+                                </span>{" "}
+                                Experience in adult services programming,
+                                web development, data management, customer
+                                service, and more.
+                            </p>
+                            <div className="wp-block-button aligncenter is-style-squared">
+                                <Link
+                                    className="wp-block-button__link"
+                                    to="/resume/"
+                                >
+                                    View my full résumé
+                                </Link>
                             </div>
                         </div>
-
-                        <div className="wp-block-columns alignwide has-2-columns">
-                            <div
-                                className={
-                                    "wp-block-column " + styles.description
-                                }
-                            >
-                                <LibraryIcon
-                                    style={{
-                                        display: "block",
-                                        margin: "auto"
-                                    }}
-                                    className="has-header-color"
-                                />
-                                <h3>Libraries</h3>
-                                <p>
-                                    Some of the projects I’ve completed while
-                                    working at a library and while completing my
-                                    MLIS.
-                                </p>
-                            </div>
-                            <div
-                                className={"wp-block-column " + styles.content}
-                            >
-                                {libraries.map((edge) => (
-                                    <Excerpt
-                                        node={edge.node}
-                                        key={edge.node.fields.slug}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-
-                        <div className="wp-block-columns alignwide has-2-columns">
-                            <div
-                                className={"wp-block-column " + styles.content}
-                            >
-                                {software.map((edge) => (
-                                    <Excerpt
-                                        node={edge.node}
-                                        key={edge.node.fields.slug}
-                                    />
-                                ))}
-                            </div>
-                            <div
-                                className={
-                                    "wp-block-column " + styles.description
-                                }
-                            >
-                                <SoftwareIcon
-                                    style={{display: "block", margin: "auto"}}
-                                    className="has-header-color"
-                                />
-                                <h3>Software</h3>
-                                <p>
-                                    These are various software projects I've
-                                    worked on.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="wp-block-columns alignwide has-2-columns">
-                            <div
-                                className={
-                                    "wp-block-column " + styles.description
-                                }
-                            >
-                                <ToolIcon
-                                    style={{
-                                        display: "block",
-                                        margin: "auto"
-                                    }}
-                                    className="has-header-color"
-                                />
-                                <h3>Woodworking</h3>
-                                <p>
-                                    Guides for the woodworking projects I’ve
-                                    done.
-                                </p>
-                            </div>
-                            <div
-                                className={"wp-block-column " + styles.content}
-                            >
-                                {woodworking.map((edge) => (
-                                    <Excerpt
-                                        node={edge.node}
-                                        key={edge.node.fields.slug}
-                                    />
-                                ))}
+                        <div className="wp-block-column">
+                            <p style={{textAlign: "center"}}>
+                                <strong>Let’s get in touch.</strong>
+                            </p>
+                            <p>
+                                Questions, comments, job offers, jokes,
+                                poems, or just to say “hi.”{" "}
+                            </p>
+                            <div className="wp-block-button aligncenter is-style-squared">
+                                <a
+                                    className="wp-block-button__link"
+                                    href="mailto:jbpjackson@icloud.com"
+                                >
+                                    <MailIcon /> Email me
+                                </a>
                             </div>
                         </div>
                     </div>
-                </main>
-            </div>
+
+                    <div className="wp-block-columns alignwide has-2-columns">
+                        <div
+                            className={
+                                "wp-block-column " + styles.description
+                            }
+                        >
+                            <LibraryIcon
+                                style={{
+                                    display: "block",
+                                    margin: "auto"
+                                }}
+                                className="has-header-color"
+                            />
+                            <h3>Libraries</h3>
+                            <p>
+                                Some of the projects I’ve completed while
+                                working at a library and while completing my
+                                MLIS.
+                            </p>
+                        </div>
+                        <div
+                            className={"wp-block-column " + styles.content}
+                        >
+                            {libraries.map((edge) => (
+                                <Excerpt
+                                    node={edge.node}
+                                    key={edge.node.fields.slug}
+                                />
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="wp-block-columns alignwide has-2-columns">
+                        <div
+                            className={"wp-block-column " + styles.content}
+                        >
+                            {software.map((edge) => (
+                                <Excerpt
+                                    node={edge.node}
+                                    key={edge.node.fields.slug}
+                                />
+                            ))}
+                        </div>
+                        <div
+                            className={
+                                "wp-block-column " + styles.description
+                            }
+                        >
+                            <SoftwareIcon
+                                style={{display: "block", margin: "auto"}}
+                                className="has-header-color"
+                            />
+                            <h3>Software</h3>
+                            <p>
+                                These are various software projects I've
+                                worked on.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="wp-block-columns alignwide has-2-columns">
+                        <div
+                            className={
+                                "wp-block-column " + styles.description
+                            }
+                        >
+                            <ToolIcon
+                                style={{
+                                    display: "block",
+                                    margin: "auto"
+                                }}
+                                className="has-header-color"
+                            />
+                            <h3>Woodworking</h3>
+                            <p>
+                                Guides for the woodworking projects I’ve
+                                done.
+                            </p>
+                        </div>
+                        <div
+                            className={"wp-block-column " + styles.content}
+                        >
+                            {woodworking.map((edge) => (
+                                <Excerpt
+                                    node={edge.node}
+                                    key={edge.node.fields.slug}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </main>
         </Layout>
     );
 }
