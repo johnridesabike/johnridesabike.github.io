4fb895cf7cf7315457c58c6ba985877606a70887
John <37978984+johnridesabike@users.noreply.github.com>
2020-04-02 08:45:00 -0400

    Updates

---
+++ component---src-pages-software-jsx-d7c211a4e2e3fd8f9490.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{oJyw:function(w,c,n){"use strict";n.r(c);var o=n("c/ce");c.default=o.b}}]);
+//# sourceMappingURL=component---src-pages-software-jsx-d7c211a4e2e3fd8f9490.js.map
