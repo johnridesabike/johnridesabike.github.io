1400ce56840dd2aa97a0e8d571439c1d2aa3e9b1
John <37978984+johnridesabike@users.noreply.github.com>
2020-07-10 11:18:35 -0400

    quickfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,3 @@ import {make} from "../pages_re/Page_Index.bs";
 import {graphql} from "gatsby";
 export default make;
 
-export const query = graphql`
-fragment PageExcerpt2 on MarkdownRemark {
-  excerpt
-}
-
-query Test {
-  markdownRemark {
-    ...PageExcerpt2
-  }
-}
-
-`;
