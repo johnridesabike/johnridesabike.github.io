67f9730214d1ba09c97dc12ad8c165b4f4408e76
John <37978984+johnridesabike@users.noreply.github.com>
2023-10-22 18:24:07 -0400

    Upgrade Acutis

--- _includes/acutisComponents.js
+++ _includes/acutisComponents.js
@@ -45,60 +45,55 @@ function makeImg(props) {
 
 const Ty = Typescheme;
 
-module.exports = [
-  Component.funAsync("Feather", Ty.make([["icon", Ty.string()]]), ({ icon }) =>
-    Promise.resolve(icons[icon].toSvg())
-  ),
+module.exports.Feather = function Feather({ icon }) {
+  return Promise.resolve(icons[icon].toSvg());
+};
+module.exports.Feather.interface = Ty.make([["icon", Ty.string()]]);
 
-  /*
-   * Sometimes this gets used in a markdown file, so the HTML output gets
-   * parsed as markdown. This means that extra lines with indentation can
-   * get parsed as a code block, which we don't want.
-   */
-  Component.funAsync(
-    "Img",
-    Ty.make([
-      ["src", Ty.string()],
-      ["alt", Ty.string()],
-      ["class", Ty.string()],
-      ["width", Ty.int()],
-    ]),
-    (props) =>
-      makeImg(props)
-        .then((props) => {
-          const className = props["class"] ? `class="${props["class"]}"` : "";
-          if (props.image.tag == "vector") {
-            const { src, width } = props.image.vector;
-            return Promise.resolve(`<img
-              src="${src}"
-              alt="${props.alt}"
-              width="${width}"
-              ${className} />`);
-          } else {
-            const [{ url, width, height }, ...rest] = props.image.images;
-            const srcset = rest
-              .map(({ url, srcset }, i) => {
-                switch (i) {
-                  case 0:
-                    return `, ${url} 1.5x`;
-                  case 1:
-                    return `, ${url} 2x`;
-                  default:
-                    return `, ${srcset}`;
-                }
-              })
-              .join("");
-            return Promise.resolve(`<img
-              srcset="${url} 1x${srcset}"
-              src="${url}"
-              width="${width}"
-              height="${height}"
-              alt="${props.alt}" ${className} loading="lazy"
-            />`);
-          }
-        })
-        .catch((e) =>
-          Promise.reject(new Error("Problem with Img: " + e.message))
-        )
-  ),
-];
+/*
+ * Sometimes this gets used in a markdown file, so the HTML output gets
+ * parsed as markdown. This means that extra lines with indentation can
+ * get parsed as a code block, which we don't want.
+ */
+module.exports.Img = function (props) {
+  return makeImg(props)
+    .then((props) => {
+      const className = props["class"] ? `class="${props["class"]}"` : "";
+      if (props.image.tag == "vector") {
+        const { src, width } = props.image.vector;
+        return Promise.resolve(`<img
+          src="${src}"
+          alt="${props.alt}"
+          width="${width}"
+          ${className} />`);
+      } else {
+        const [{ url, width, height }, ...rest] = props.image.images;
+        const srcset = rest
+          .map(({ url, srcset }, i) => {
+            switch (i) {
+              case 0:
+                return `, ${url} 1.5x`;
+              case 1:
+                return `, ${url} 2x`;
+              default:
+                return `, ${srcset}`;
+            }
+          })
+          .join("");
+        return Promise.resolve(`<img
+            srcset="${url} 1x${srcset}"
+            src="${url}"
+            width="${width}"
+            height="${height}"
+            alt="${props.alt}" ${className} loading="lazy"
+          />`);
+      }
+    })
+    .catch((e) => Promise.reject(new Error("Problem with Img: " + e.message)));
+};
+module.exports.Img.interface = Ty.make([
+  ["src", Ty.string()],
+  ["alt", Ty.string()],
+  ["class", Ty.string()],
+  ["width", Ty.int()],
+]);
